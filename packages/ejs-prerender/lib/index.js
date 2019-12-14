c3dfebc12d4ef63938517ee2f0e3423dd1815711
David Ragsdale <me@davidjragsdale.com>
2019-12-14 10:57:44 -0500

    Expose getBaseComponentsDir function

--- packages/ejs-prerender/lib/index.js
+++ packages/ejs-prerender/lib/index.js
@@ -1,4 +1,5 @@
 const ejsPrerender = require('./ejsPrerender');
+const getBaseComponentsDir = require('./getBaseComponentsDir');
 const getConfig = require('./getConfig');
 const grabPages = require('./grabPages');
 const grabPagesSync = require('./grabPagesSync');
@@ -6,6 +7,7 @@ const renderTemplate = require('./renderTemplate');
 const replaceExtension = require('./replaceExtension');
 const writeHtml = require('./writeHtml');
 
+ejsPrerender.getBaseComponentsDir = getBaseComponentsDir;
 ejsPrerender.getConfig = getConfig;
 ejsPrerender.grabPages = grabPages;
 ejsPrerender.grabPagesSync = grabPagesSync;
